2c02a6940873cf7f6e84f11c1fe28db84599358c
lworku <lydiasgthb@gmail.com>
2019-08-09 20:28:03 -0700

    inital commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,7 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-
+ 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
